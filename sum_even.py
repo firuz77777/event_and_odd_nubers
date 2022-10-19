543c25d0e089a41dd74ba3fe7355b50ea60507a9
firuz77777 <namefiruzrahmiddinov33@gmail.com>
2022-10-19 18:11:28 +0500

    4

--- sum_even.py
+++ sum_even.py
@@ -4,7 +4,8 @@ x1 = (var_int // 1000 + 1) % 2 * (var_int // 1000)
 x2 = (var_int % 1000 // 100 + 1) % 2 * (var_int % 1000 // 100)
 x3 = (var_int % 1000 % 100 // 10 + 1) % 2 * (var_int % 1000 % 100 // 10)
 x4 = (var_int % 10 + 1) % 2 * (var_int % 10)
-sum_even = x1 + x2 + x3 + x4
+sum_even=x1+x2+x3+x4
+
 #Create a variable "var_int" and assign it a four-digit integer value.
 
 #Create a variable "sum_even" and assign it 0.
