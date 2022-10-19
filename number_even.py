1190ea1eed572fffe3ed82e1522223c700a02229
firuz77777 <namefiruzrahmiddinov33@gmail.com>
2022-10-19 15:47:06 +0500

    juft

--- number_even.py
+++ number_even.py
@@ -1,9 +1,9 @@
 #A four-digit integer is given. Find the number of even digits in it.
-a = 2468
-x1 = (a // 1000 + 1) % 2
-x2 = (a % 1000 // 100 +1) % 2
-x3 = (a % 1000 % 100 // 10 + 1) % 2
-x4 = (a % 1000 % 100 % 10 + 1) % 2
+var_int = 2468
+x1 = (var_int // 1000 + 1) % 2
+x2 = (var_int % 1000 // 100 +1) % 2
+x3 = (var_int % 1000 % 100 // 10 + 1) % 2
+x4 = (var_int % 10 + 1) % 2
 print(x1+x2+x3+x4)
 #Create a variable "var_int" and assign it a four-digit integer value.
 
